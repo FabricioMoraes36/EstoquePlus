dc55e413fbe55a3f871a9535e8c677beff67edbe
Fabricio Moraes <Fabricio36365@outlook.com>
2025-09-27 09:30:28 -0300

    first commit

---
+++ src/main/java/com/group/EstoquePlus/EstoquePlusApplication.java
@@ -0,0 +1,13 @@
+package com.group.EstoquePlus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstoquePlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstoquePlusApplication.class, args);
+	}
+
+}
