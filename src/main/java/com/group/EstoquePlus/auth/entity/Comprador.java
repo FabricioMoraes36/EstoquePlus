fd72aedcf5ff9a1055bcda318a511b41097128e9
rafaeldossanto <rafael.santoss2802@gmail.com>
2025-09-27 21:22:25 -0300

    esqueci do manytomany no Comprador

--- src/main/java/com/group/EstoquePlus/auth/entity/Comprador.java
+++ src/main/java/com/group/EstoquePlus/auth/entity/Comprador.java
@@ -1,13 +1,18 @@
 package com.group.EstoquePlus.auth.entity;
 
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ManyToMany;
 import jakarta.validation.constraints.Email;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.validator.constraints.br.CPF;
+
+import java.util.List;
 
 @Entity
 @Setter
@@ -18,4 +23,7 @@ public class Comprador {
     private String nome, senha;
     private CPF cpf;
     private Email email;
+
+    @ManyToMany(mappedBy = "endereco", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Endereco> enderecos;
 }
